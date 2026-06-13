9c966158469fe5a344dc0a607c91a5c81d8c7c3e
Dev Saini <149680766+devsaini889@users.noreply.github.com>
2026-06-13 15:44:03 +0530

    Delete GenAI_Interview_mentor directory

--- GenAI_Interview_mentor/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-const authRoutes = require('./routes/auth.routes');
-app.use('/api/auth', authRoutes);
-
-module.exports = app;
--- GenAI_Interview_mentor/src/routes/auth.routes.js
+++
@@ -1,5 +0,0 @@
-const {Router} = require('express');
-const UserModel = require('../models/user.model');
-const router = Router();
-
-module.exports = router;
